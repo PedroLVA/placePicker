e38e2f91a4be41235477d17be1d1a79bcc725f77
Pedro Athayde <pedro_athayde@outlook.com>
2024-07-12 11:35:20 -0300

    added delete method and interceptor

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -3,6 +3,7 @@ import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
 import { PlacesService } from '../places.service';
+import { Place } from '../place.model';
 
 @Component({
   selector: 'app-user-places',
@@ -37,6 +38,15 @@ export class UserPlacesComponent implements OnInit{
       subscription.unsubscribe();
     })
   }
+
+  onRemovePlace(place: Place){
+    const subscription = this.placesService.removeUserPlace(place).subscribe();
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+  
+  }
+  
 }
 
 
