39aefafd1a25367ae08961d336f0cb4fedef3dd4
Pedro Athayde <pedro_athayde@outlook.com>
2024-07-12 11:40:37 -0300

    Response interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpEventType, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 function loggingInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn){
-    console.log('[Outgoing Request]')
-    console.log(request)
-    return next(request)
+    console.log('[Outgoing Request]');
+    console.log(request);
+    return next(request).pipe(
+        tap({
+            next: event => {
+                if(event.type === HttpEventType.Response){
+                    console.log('[incoming response]')
+                    console.log(event.status)
+                    console.log(event.body)
+                }
+            }
+        })
+    );
 }
 
 bootstrapApplication(AppComponent, {
